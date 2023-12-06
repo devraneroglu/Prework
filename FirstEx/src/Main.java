90d7111634f6a94dd9770fd8626917d3c7de7baf
Devran EROĞLU <devraneroglu@gmail.com>
2023-12-06 21:38:29 +0300

    first commit

---
+++ FirstEx/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Patika.dev FullStack Java Spring boot & React Bootcamp");
+        System.out.println("\033[4mPatika.dev\033[0m FullStack Java Spring Boot & React Bootcamp");
+    }
+}
